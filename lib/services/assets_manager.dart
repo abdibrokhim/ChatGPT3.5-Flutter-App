8164479feca27c0af306b57d43d57d7f6aba2678
abdibrokhim <abdibrokhim@gmail.com>
2023-04-10 16:59:26 +0500

    init

---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,5 @@
+class AssetsManager {
+  static String userImage = "images/person.png";
+  static String botImage = "images/chat_logo.png";
+  static String openaiLogo = "images/openai_logo.jpg";
+}
